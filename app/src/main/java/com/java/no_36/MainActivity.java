c17640713bf6d05cb30df9d204a205fee7cf246c
2639594603@qq.com <2639594603@qq.com>
2017-09-04 11:26:13 +0800

    menu

--- app/src/main/java/com/java/no_36/MainActivity.java
+++ app/src/main/java/com/java/no_36/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_collect) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_night) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_classify) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_text) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_shield) {
 
         }
 
