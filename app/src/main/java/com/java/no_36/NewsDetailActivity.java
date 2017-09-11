111a3e5f0f3f92933b28048e583db3491960bbde
zhanghuimeng <zhanghuimeng1997@gmail.com>
2017-09-11 21:06:44 +0800

    改了一下新闻详情界面

--- app/src/main/java/com/java/no_36/NewsDetailActivity.java
+++ app/src/main/java/com/java/no_36/NewsDetailActivity.java
@@ -8,6 +8,9 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by admin on 2017/9/7.
  */
@@ -41,9 +44,28 @@ public class NewsDetailActivity extends AppCompatActivity
             title.setText(bean.getNews_title());
 
             TextView content = (TextView) findViewById(R.id.detail_content);
-            content.setText(bean.getNews_content());
+            // content.setText(bean.getNews_content());
+            String result = processSeggedContent(bean.getSegged_p_list_of_content());
+            if (result != null)
+                content.setText(result);
         };
     };
+
+    private String processSeggedContent(String seggedCont)
+    {
+        JSONArray json;
+        String content;
+        try
+        {
+            json = new JSONArray(seggedCont);
+            content = json.getString(0);
+        }
+        catch (Exception e) { e.printStackTrace(); return null; }
+
+        content = content.replaceAll("/[a-zA-Z]+\\s\\s", "");
+        // content = seggedCont;
+        return content;
+    }
 
     protected void onCreate(Bundle savedInstanceState)
     {
